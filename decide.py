c919854be92aebe9b071e2924b5cd7e7f3c24ae2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-15 18:13:02 +0100

    Update decide.py

--- decide.py
+++ decide.py
@@ -1,5 +1,79 @@
-def decide_el_conteneur():
-    pass
-#sur l'age le sexe
 #on remplis la case wiki par une image gnre salut emoticone !
-#vu que c grave pas ok ben go remonter le truk avec ca et puis c cool ^^
+
+from conteneur import *
+from conteneur_rep import *
+
+
+
+def q_sans_rien(entree):
+    conteneur()
+    conteneur_reponse1()
+
+    espace = ""
+
+
+    for i in entree:
+        if i == " ":
+            espace = True
+
+        else:
+            espace = False
+
+    return espace
+    #si y'a un espace alors y'a un autre truk apres
+    
+def q_vous(entree):
+
+    politesse = ""
+    
+    vous = str(entree).find("vous")
+    if vous >= 0:
+        politesse = True
+
+        
+    return politesse
+    #si use politesse alors use polistesse
+
+
+    
+def rep_bonjour(entree):
+
+    bj = ""
+    
+    bonjour = str(entree).find("bonjour")
+    if bonjour >= 0:
+        bj = True
+
+
+    return bj
+    #si bonjour alors on rep bonjour sinon autre hasard
+
+
+def fin_debut_de_conversation():
+    pass
+
+
+
+
+
+
+
+
+
+def rep_bonjour(entree):
+    pass
+def rep_sans_question(entree):
+    pass
+
+def rep_avec_question(entree):
+    pass
+
+
+def toutes_les_3phrases():
+    pass
+#return blagounettte genre un truk rien a voir et on dis oups trompé
+
+def enregistrement():
+    pass
+#dans un fichier comme ca on recup la convers
+
