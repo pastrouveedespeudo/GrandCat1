a97ca2a6166c8375ace1031e752bc4b4e7b2eaa7
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-15 18:13:26 +0100

    Update conteneur.py

--- conteneur.py
+++ conteneur.py
@@ -1,5 +1,178 @@
-conteneur():
+def conteneur():
 
-    bonjour = ["bonjour", "salut", ""]
-    reponse_bonjour = [""]
+    bonjour = ["bonjour", "salut", "coucou","hey",
+               "cuocou", "hello", "heyy", "heyyy",
+               "kikou", "slt","salt", "bnjour",
+               "bonjour !"
+               ]
+
+    bonjour_politesse_pas_vous = ["bonjour ca va ?",
+                                  "bonjour comment ca va ?",
+                                  "bonjour comment vas tu ?",
+
+                             
+                             "salut ca va ?",
+ 
+                             "salut comment ca va ?",
+                             "salut comment vas tu ?",
+
+                             "coucou ca va ?",
+
+                             "coucou comment ca va ?",
+
+                             "coucou comment vas tu ?",
+                             
+                             "hey ca va ?",
+                             "hey comment ca va ?",
+
+
+                             "hey comment vas tu ?",
+                             
+                             "cuocou ca va ?",
+
+                             "cuocou comment ca va ?",
+
+
+                             "cuocou comment vas tu ?",
+
+
+                             "hello ca va ?",
+
+                             "hello comment ca va ?",
+
+                                  
+                             "hello comment vas tu ?",
+
+
+                             "heyy  ca va ?",
+
+                             "heyy comment ca va ?",
+
+                             "heyy comment vas tu ?",
+         
+                             "heyyy  ca va ?",
+
+                             "heyyy comment ca va ?",
+
+                                  
+                             "heyyy comment vas tu ?",
+
+                             "kikou  ca va ?",
+
+
+
+
+
+                             "kikou comment ca va ?",
+
+                             "kikou comment vas tu ?",
+
+                             "slt  ca va ?",
+
+                             "slt comment ca va ?",
+
+                             "slt comment vas tu ?",
+
+                             "bnjour  ca va ?",
+
+                                  
+
+                             "bnjour comment ca va ?",
+
+                             "bnjour comment vas tu ?",
+                   ]
+
+    bonjour_politesse_vous = [
+
+                             "bonjour comment allez vous ?",
+                             "bonjour vous allez bien ?",
+
+                            "coucou comment allez vous ?",
+                            "salut vous allez bien ?",
+                            "salut comment allez vous ?",
+                             "coucou vous allez bien ?",
+                             "hey comment allez vous ?",
+                             "hey vous allez bien ?",
+                             "cuocou comment allez vous ?",
+                             "cuocou vous allez bien ?",
+
+                             "heyyy vous allez bien ?",
+                             "heyy vous allez bien ?",
+                             "hello vous allez bien ?",
+                             "hello comment allez vous ?",
+                             "heyy comment allez vous ?",
+                             "heyyy comment allez vous ?",
+                             "kikou comment allez vous ?",
+                             "kikou vous allez bien ?",
+
+                             "slt vous allez bien ?",
+                             "slt comment allez vous ?",
+                             "bnjour comment allez vous ?",
+                             "bnjour vous allez bien ?",
+
+
+
+                             
+
+            ]
+
+
+
     
+   
+
+                       
+                       
+    return bonjour, bonjour_politesse_pas_vous, bonjour_politesse_vous,\
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
