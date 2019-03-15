db6d0bd9f7921c6c4ceb9d52c94ac0153cea417a
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-15 16:07:17 +0100

    Add files via upload

---
+++ decide.py
@@ -0,0 +1,5 @@
+def decide_el_conteneur():
+    pass
+#sur l'age le sexe
+#on remplis la case wiki par une image gnre salut emoticone !
+#vu que c grave pas ok ben go remonter le truk avec ca et puis c cool ^^
