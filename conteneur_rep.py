7cd205babd9444beeaeae47fc973b80dd98494da
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-15 18:13:55 +0100

    Add files via upload

---
+++ conteneur_rep.py
@@ -0,0 +1,32 @@
+
+def conteneur_reponse1():
+
+    reponse_bonjour = [
+
+                       "Bonjour comment vas tu ?",
+                       "Bonjour comment ca va ?",
+                       "Bonjour ca va ?",
+                       "Bonjour comment tu vas ?",
+                       "Bonjour !",
+                       ]
+
+
+    
+    reponse_salut = ]
+                       "salut comment vas tu ?",
+                       "salut comment ca va ?",
+                       "salut ca va ?",
+                       "salut comment tu vas ?",
+                       "salut ! ",
+                       ]
+
+
+                       
+    reponse_bonjour_vous = ["Bonjour comment allez vous ?",
+                           "Bonjour vous allez bien ?",
+                           "salut comment allez vous ?",
+                           "salut vous allez bien ?",
+
+                        ]
+                       
+
