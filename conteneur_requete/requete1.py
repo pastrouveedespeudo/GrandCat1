f62439d1d2fc81afd4d82bdf29defdd5bf3bf550
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-17 22:14:48 +0100

    Update requete1.py

--- conteneur_requete/requete1.py
+++ conteneur_requete/requete1.py
@@ -1,1 +1,31 @@
-a
+def requete1():
+
+
+    liste = [["salut",
+             "ca va ?",
+             "ouais et toi ?",
+             "ouais super merci !",],
+
+            ["hey comment ca va ?",
+             "ca va bien merci et toi ?",
+             "ca va aussi !",],
+
+
+
+
+            ["salut ca va ?",
+             "ouais et toi ?",
+             "ca va !",],
+
+
+
+
+
+           ["salut ca va ?",
+            "oui et toi ?",
+            "oui",],
+
+
+             ]
+
+    return liste
