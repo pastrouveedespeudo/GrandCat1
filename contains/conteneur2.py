5a6ed0e55dc9eb3bceb3cf2d4c46dcfc53a0c0b2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-17 19:16:14 +0100

    Add files via upload

---
+++ contains/conteneur2.py
@@ -0,0 +1,22 @@
+def conteneur2():
+
+
+    
+    oui = ["oui merci et toi ?",
+            "tres bien merci et toi ?",
+            "super et toi ?",
+            "je vais bien merci et toi ?",
+            "ca va et toi ?",
+            "ouais ca va et toi ?",
+            "ca va supeeeer et toi ?",
+  
+
+
+
+        ]
+
+   
+    
+    return oui
+
+    
