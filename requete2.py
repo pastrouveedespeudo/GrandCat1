cd58412372bfbbd7e20e55831a5098390af221dd
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-18 20:29:18 +0100

    Add files via upload

---
+++ requete2.py
@@ -0,0 +1,28 @@
+def requete2():
+
+    liste = [["Chavais tu que les chats avaient une patte dominante ?",
+              "Les males ont pour patte domminante la gauche, et les femelles\
+              la droite !",#non
+              "sérieux ? t'es un dingue toi !"#oui
+              ],
+             ["coucou",
+
+
+                 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ]
+
+    return liste
