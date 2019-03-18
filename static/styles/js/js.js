c56167e811c7d422894a9e2af79fac2d967ca6da
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-18 19:11:34 +0100

    Update js.js

--- static/styles/js/js.js
+++ static/styles/js/js.js
@@ -6,71 +6,49 @@ var liste_image_logo = ["/static/images/loading/one.jpg",
                       "/static/images/loading/two.jpg",
                       "/static/images/loading/three.jpg"];
 
-
 var liste_image_logo_i = 0
 
 function change_image(){
-document.getElementById('tourne').src = liste_image_logo[liste_image_logo_i];
-if(liste_image_logo_i < liste_image_logo.length - 1){
-  liste_image_logo_i++
-}
-else{
-  liste_image_logo_i = 0
-}
+    document.getElementById('tourne').src = liste_image_logo[liste_image_logo_i];
+    if(liste_image_logo_i < liste_image_logo.length - 1){
+        liste_image_logo_i++
+    }
+    else{
+        liste_image_logo_i = 0
+    };
 };
-
 
 
 var time;
 function start_logo_loading(){
-time = window.setInterval("change_image()", 1000);
-}
+    time = window.setInterval("change_image()", 1000);
+};
 
 
 function stop_logo_loading(){
-clearTimeout(time);
-}
-
-
-
-
-
-
-
+    clearTimeout(time);
+};
 
 
 function map(){
-
-
-  document.getElementById("map").innerHTML = ""
-  document.getElementById("map").style.width = "100%"
-  document.getElementById("map").style.height = "200px"
-
-
-}
-
-
+  document.getElementById("map").innerHTML = "";
+  document.getElementById("map").style.width = "100%";
+  document.getElementById("map").style.height = "200px";
+};
 
 
 MONCADRE = []
 
 $(document).ready(function(){
-  
   $("form").on("submit", function(e){
-      start_logo_loading();
       $.ajax({
           data:{
               data:$("#idRecupInfo").val(),
-
           },
           type:"POST",
           url:"/data"
-
-          
-
       })
       .done(function(data){
-
           if (data.error){
               $("#monCadreAlert").text(data.error);
               $("#monCadre");
@@ -79,42 +57,28 @@ $(document).ready(function(){
               $("#monCadre").html(data.data);
               $("#monCadreAlert");
               initMap();
-              
           };
-
-
       });
-      
-      
       add_favorite();
       e.preventDefault();
-      map();
-      
+      map(); 
   });
 });
 
 
-
-
 MONCADRE_WKI = []
-
 $(document).ready(function(){
   var finish2 = []
+  start_logo_loading();
   $("form").on("submit", function(ev){
-
       $.ajax({
           data:{
               data:$("#idRecupInfo").val(),
-
           },
           type:"POST",
           url:"/wiki"
-          
-
       })
-      
       .done(function(data){
-
           if (data.error){
               $("#monCadreAlert").text(data.error);
               $("#monCadreWiki");
@@ -122,18 +86,13 @@ $(document).ready(function(){
           else{
               $("#monCadreWiki").html(data.data);
               $("#monCadreAlert");
-              stop_logo_loading();
-              
+              stop_logo_loading(); 
           }
       });
-      
       effacer();
       ev.preventDefault();
-  
   });
 });
-
-
 
 
 
@@ -144,107 +103,36 @@ var ask = '  <center>Voulez vous ajouter cette adresse à vos favoris ?</center>
             <input type="submit" class="classButtonInput" id="idButtonInput" value="Non"/> \
             <form>'
 
-
-document.getElementById("divFavorite").innerHTML = ask
-//ici faire un post
-//et document.getElementById("divFavorite").innerHTML = ""
-
+    document.getElementById("divFavorite").innerHTML = ask;
 };
 
 
 
-
 function initMap(){
-  
-  liste = [[],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[],
-           [],[],[],[],[],[],[],[]]
-  
-  liste2 = []
-  
-  var c = document.getElementById("monCadre");
-
-  var a = c.innerText || c.textContent;
-  var b = a.length;
-  //console.log(a)
-  
-  var c = 0;
-
-  for(var i = 0; i<=b;i++){
-    liste[c].push(a[i]);
-    if(a[i] === ","){
-        c++;
-        };
-  };
-
-  for(var i =0;i<=liste.length;i++){
-      if(liste[i]!=""){
-          liste2.push(liste[i]);
-      }
-   };
-  console.log(liste2)
-  a = Number(liste2[liste2.length -3].slice(1,-1).join(''));
-  //b = Number(liste2[liste2.length -2].slice(2,-5).join(''));
-  b = Number(liste2[liste2.length -2].slice(1,-4).join(''));
-  //console.log(liste2)
-  console.log(a)
-  console.log(b)
-  //console.log(liste2.length -2)
-
-
+  liste = []
+  var c = document.getElementById("monCadreHidden");
+  var d = document.getElementById("monCadreHidden2");
+  var monTexte = c.innerText || c.textContent;
+  var monTexte2 = d.innerText || d.textContent;
+  var a = Number(monTexte);
+  var b = Number(monTexte2);
   var options = {
       zoom:13,
       center:{lat:a,lng:b}
   }
   var map = new google.maps.Map(document.getElementById("map"), options);
-} 
-
-
+  stop_logo_loading();
+}; 
 
 
 
 function effacer(){
   document.getElementById("idRecupInfo").value = "";
-}
-
+};
 
 
 function imageTourne(){
   document.getElementById('nom-de-ta-photo').src= "l'url de ta nouvelle image";
-}
-
+};
 
 
