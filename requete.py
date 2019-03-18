cd50f15f297462f038a1f278bc5183a5f521e24c
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-18 20:29:04 +0100

    Update requete.py

--- requete.py
+++ requete.py
@@ -1,3 +1,39 @@
-def phrase():
-    pass
-    a = [['bonjour'],]
+def requete1():
+
+
+    liste = [["salut",
+             "ca va ?",
+             "ouais et toi ?",
+             "ouais super merci !",],
+
+            ["hey comment ca va ?",
+             "ca va bien merci et toi ?",
+             "ca va aussi !",],
+
+
+
+
+            ["salut ca va ?",
+             "ouais et toi ?",
+             "ca va !",],
+
+
+
+
+
+           ["salut ca va ?",
+            "oui et toi ?",
+            "oui",],
+
+
+
+
+           ["salut ca va ?",
+            "oui et toi ?",
+            "oui",],
+
+
+             ]
+
+
+    return liste
