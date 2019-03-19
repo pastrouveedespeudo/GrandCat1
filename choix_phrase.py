f55187cb7d9ccc9e298d6d2ce98b4d1478aec99a
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-19 11:11:22 +0100

    Update choix_phrase.py

--- choix_phrase.py
+++ choix_phrase.py
@@ -3,7 +3,9 @@ from .requete2 import *
 import os
 import random
 
-def lecture_liste(entree):
+
+
+def lecture_liste(entree, liste_oui_non):
 
     c = 0
     liste = requete1()
@@ -13,12 +15,22 @@ def lecture_liste(entree):
         for j in liste[c]:
             #print(liste[c][c1])
             if entree == liste[c][c1]:
-                phrase_apres.append(liste[c][c1 + 1])
+                try:
+                    phrase_apres.append(liste[c][c1 + 1])
+                except:
+                    phrase_apres.append("faut mettre d'autre phrassse")
             c1+=1
         c+=1
+   
+    #liste_oui_non.append("reponse schéma")
 
-    return phrase_apres
-
+    try:
+        dico_best_phrase = tri_liste(phrase_apres)
+        best_phrase = analyse_dico(dico_best_phrase, liste_oui_non)
+    except:
+        best_phrase = "PLUS DE PHRASEE"
+    
+    return best_phrase
 
 
 def tri_liste(entrance):
@@ -45,32 +57,62 @@ def tri_liste(entrance):
                 dico[i] += 1
     return dico
 
-
-
-def analyse_dico(entree, premiere_entree):
+def analyse_dico(entree, liste_oui_non):
 
     try:
         dico_best = max(entree)
-        return dico_best, " "
+        liste_oui_non.append("réponse schéma")
+        return dico_best
     except:
-        dico_best = dico_best_empty(premiere_entree)
+        dico_best = dico_best_empty(entree, liste_oui_non)
         return dico_best
 
 
 
+
+
+def dico_best_empty(entree, liste_oui_non):
+
+    #choix_aleatoire = random.choice(3)
+    
+    #1 une anecdote
+    choix_aleatoire = 1
+    if choix_aleatoire == 1:
         
+        reponse = requete2()
+        
+        longueur_conteneur2 = len(reponse) - 1
+        hasard = random.randint(0, longueur_conteneur2)
+
+        liste_oui_non.append("phrase anecdocte "+str(hasard))
+
+        print(reponse[hasard][0],"POZINI BUTTTTTTTT")
+
+
+
+
+        return reponse[hasard]
+
+
+def oui_non(oui_non, entree):
+    cont = requete2()
     
 
+    liste_oui = ["oui"]
+    liste_non = ['non']
+
+    conteneur_rep = cont[int(entree[-1])]
+    
+    for i in liste_oui:
+        if oui_non == i:
+            return conteneur_rep[1], "fin anecdocte"
+
+        
+    for i in liste_non:
+        if oui_non == i:
+            return conteneur_rep[2], "fin anecdocte"
 
 
-def dico_best_empty(entree):
-    print(entree)
-
-    #1 une anecdote
-    #random.choice(3)
-
-    a = cont2()
-    return a
 
 
     #2 une question
@@ -86,28 +128,6 @@ def dico_best_empty(entree):
     #4 oui dis moi et on marque la prochaine phrase
     #on doit ajouter " "
 
-def cont2():
-    reponse = requete2()
-
-    longueur_conteneur2 = len(reponse) -1
-    hasard = random.randint(0, longueur_conteneur2)
-
-    reponse[hasard]
-    
-    return reponse[hasard][0], "cont2"
-
-
-def oui_non(oui_non, entree):
-    pass
-    #on recup "cont2" on recup lentree et si le type a dit oui ou non
-
-
-#soit faire un systeme de liste avec le conteneur appropriere
-#on reserve les oui, oui je saivais ouais et non je savais pas
-#et on met if question1 == oui ect
-#on reprend le dernier conteneur et on on met +1 pour non + 2 pour oui
-
-#ohhhhhhh bonneuh mere y'a american gods
 
 
 
@@ -117,32 +137,6 @@ def oui_non(oui_non, entree):
 
 
 
-
-
-
-
-
-
-def ecriture():
-
-    dernier_fichier = []
-    with open("liste_phrase.py","r") as file_phrase:    #en gros la on liste les fichier de phrase
-        dernier_fichier.append(file_phrase)
-
-
-    new = dernier_fichier[-4:-3]
-    new = str(new + 1) + ".py"
-
-    with open(new, "a") as file:            #on ecrit le dernier titre + 1
-        file.write(("    a = [['" + str(question1) + "'],"))
-        
-
-
-
-        file.write(']')
-
-def sens_phrase():
-    pass
 
 
 
