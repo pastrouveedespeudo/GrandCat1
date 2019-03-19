c32310911bc630bf29edb526fa3bc068e4a0ce56
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-19 15:54:49 +0100

    Add files via upload

---
+++ ecriture.py
@@ -0,0 +1,35 @@
+from .requete1 import *
+import os
+
+def ecriture(entree):
+
+
+    liste = []
+
+    fichier = open("fbapp/requete1.py","r")
+
+    liste.append(fichier.read())
+
+    print(liste[0])
+    print("-----------------")
+    print(liste[0][-19:])
+    liste[0] = liste[0][:-17]
+    print(liste)
+
+
+    
+    with open("fbapp/requete1.py", "w") as file:
+        file.write((str(liste[0][:-17])\
+                    + "\n"\
+                    + "           [" + str(entree)\
+                    +"," + "\n"\
+                    + "           ]"))
+
+
+
+
+
+
+
+
+
