597930650f2c7a8550b291e594bd88309138b4d2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-19 12:00:55 +0100

    Update requete2.py

--- requete2.py
+++ requete2.py
@@ -1,14 +1,16 @@
 def requete2():
 
     liste = [["Chavais tu que les chats avaient une patte dominante ?",
-              "Les males ont pour patte domminante la gauche, et les femelles\
-              la droite !",#non
+              "Les males ont pour patte domminante la gauche, et les femelles la droite !",#non
               "sérieux ? t'es un dingue toi !"#oui
               ],
-             ["coucou",
 
+             
+             ["savez tu que les chats pousse un miolement aigu et tres court pour dire bonjour ca va?",
+              "l'ecoute c'est le plus important ! merde toi tu va finir vieux garcon",#non
+              "ah ouais ? et ca te servira a quoi dans la vie ?",#oui
 
-                 ]
+              ]
 
 
 
