68fb6ffe79fce49f8b8ad625f19640bb395a6d49
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-25 14:45:30 +0100

    Add files via upload

---
+++ test_wiki.py
@@ -0,0 +1,24 @@
+import fbapp.wikipedia_jury as script
+
+
+def test_wki():
+    parametre = "Cité Paradis"
+    sortie = "Cité Paradis"
+
+    assert script.wikipédia(parametre) == sortie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
