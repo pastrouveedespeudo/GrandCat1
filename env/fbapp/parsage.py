fc39e78477a51cc421ecdf65ae71f49c0267ebc9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-25 22:22:18 +0100

    Update parsage.py

--- env/fbapp/parsage.py
+++ env/fbapp/parsage.py
@@ -1,166 +1,142 @@
-import requests
+"""This is methods to check the answers"""
 
-from urllib.request import Request, urlopen
+import requests
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 
+
+def no_ponctuation(entrance):
+    """here we searching punctuation, and we erase it"""
+
+    new = [i for i in entrance if i != "?" and i != "!" and i != "."]
+
+    try:
+        if new[-1] == " ":
+            return "".join(new[:-1])
+        else:
+            return "".join(new)
+    except:
+        pass
+        #new empty user click into enter without input ?
+
+    return "".join(new)
+
+
 def search_dico(data):
+    """Here we search substantive of the next word"""
 
-    path = "http://www.cnrtl.fr/definition/{}/substantif".format(data)
+    path = "http://www.cnrtl.fr/definition/{}/substantif"
+    path = path.format(data)
     
-    requete = requests.get(path)
-    page = requete.content
-    
-    soup = BeautifulSoup(page, "html.parser")      
-    propriete = soup.find_all("div")
+    request = requests.get(path)
+    page = request.content
+    soup = BeautifulSoup(page, "html.parser")
+    propertyy = soup.find_all("div")
 
-    fem = str(propriete).find("fém")
-    masc = str(propriete).find("masc")
+    fem = str(propertyy).find("fém")
+    masc = str(propertyy).find("masc")
 
     if fem >= 0:
-        
         return "nf"
     elif masc >= 0:
-
         return "nm"
     else:
         return "nm"
 
 
 def apostrohpe(data):
+    """Here we deleting apostrophe and add e or a from search_dico"""
 
-   
+    list_dico = [[], [], [], [], [], [], [], [], [], [], [], [], [], []]
+    catchphrase = "Salut GrandPY  Est-ce que tu connais l'adresse"
 
-    liste = []
-    liste_dico = [[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    phrase_accroche = "Salut GrandPY ! Est-ce que tu connais l'adresse"
-    
-    a = str(data).find(str(phrase_accroche))
+    a = str(data).find(str(catchphrase))
     
     if a >= 0:
-        
-        liste = []
-        for i in data:        
-            liste.append(i)
-      
+        liste = [j for i in data for j in i]
 
-        mot = []
+        word = []
         c = 0
         com = 0
+
         for i in liste:
             if liste[c] == "'":
-
                 for j in liste[c+1:-1]:
-                    liste_dico[com].append(j)
+                    list_dico[com].append(j)
                     if j == " ":
                         com += 1
                         break
-                for i in liste_dico:
+          
+                for i in list_dico:
                     if i == []:
                         pass
                     else:
-                        sub = search_dico("".join(i))
-                        if sub == "nm":
-                            liste[c] = "e"
-              
-                        elif sub == "nf":
-                            liste[c] = "a"
-                        
-                            
-                mot.append(liste[c])
-                mot.append(" ")  
+                        try:
+                            sub = search_dico("".join(i))
+                            if sub == "nm":
+                                liste[c] = "e"
+                            elif sub == "nf":
+                                liste[c] = "a"
+                            else:
+                                liste[c] = "e"
+                        except:
+                            print("!!!!!!!!!!!!!!!!\
+                                    le dictionnaire en ligne n'a pas répondu\
+                                    Une tentative de connexion a échoué car\
+                                    le parti connecté n’a pas répondu\
+                                    convenablement au-delà d’une certaine\
+                                    durée ou une connexion établie a échoué\
+                                    car l’hôte de connexion n’a pas répondu")
+  
+                word.append(liste[c])
+                word.append(" ")
+
             else:
-                mot.append(liste[c])    
+                word.append(liste[c])
+            c += 1
+        word = "".join(word)
+        return word
+    else:
+        return data
+
+
+def parsing_text(data):
+    """Here we'll go to parsing data \
+    if user input sentences: Salut GrandPY ! Est-ce que tu connais l'adresse de\
+    we juste take the last word from the sentence"""
+
+    catchphrase = "Salut GrandPY  Est-ce que tu connais la adresse"
+    search = str(data).find(str(catchphrase))
+
+    if search >= 0:
+
+        separate_element = data.split()
+        list_into_list = [i.split() for i in separate_element]
+        join_list = [",".join(j) for i in list_into_list for j in i]
+        unification = str(join_list[-1]).replace(",","")
                 
-            c+=1
-
-        mot = "".join(mot)
-
-        return mot
+ 
+        return unification
 
     else:
         return data
 
 
-
-
-def parsing_texte(data):
-    """Here we'll go to parsing data"""
-    """if user input sentences: Salut GrandPY ! Est-ce que tu connais l'adresse de"""
-    """we juste take the last word from the sentece"""
-
-
-
-
-    liste = []
-    liste2 = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
-
-    yoda = []
-    
-    c = 0
-    
-    phrase_accroche = "Salut GrandPY ! Est-ce que tu connais la adresse"
-    a = str(data).find(str(phrase_accroche))
-
-    if a >= 0 :
-
-        for i in data:
-            liste2[c].append(i)
-            if i == " ":
-                c+=1
-
-        for j in liste2:
-            if j == []:
-                pass
-            else:
-                yoda.append("".join(j))
-     
-        return yoda[-1]
-
-    else:
-        
-        return data
-
-
-
-
-def searching(parametre):
-    """Here we searching from Python modul(geopy.geocoders)"""
-    """address from the input from html page"""
+def searching(parameter):
+    """Here we searching from Python modul(geopy.geocoders)\
+    address from the input from html page"""
 
     geocoder = Nominatim(user_agent="run.py")
     #parametre is data recup from data()
-    
-    location = geocoder.geocode(parametre, True, 30)
-    localisation = location.address
-    localisation = str(localisation)
+    try:
+        location = geocoder.geocode(parameter, True, 30)
+        localisation = location.address
+        localisation = str(localisation)
 
-    #define data from geopy.geocoders into var
-    a = location.address
-    b = location.latitude
-    c = location.longitude
-
-    return a, b, c
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        #define data from geopy.geocoders into var
+        address = location.address
+        latitude = location.latitude
+        longitude = location.longitude
+        return address, latitude, longitude
+    except:
+        return "None"
